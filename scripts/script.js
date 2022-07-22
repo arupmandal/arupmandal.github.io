9935b2d2c9911cb99723980c1dfa8ab2b44552f9
Arup Mandal <40239193+arupmandal@users.noreply.github.com>
2022-07-22 15:41:20 +0530

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+<!--- 
+    error_text: Message to display if there is an error.
+    success_text: Message to display if the form is successfully submitted.
+    disable_waittime: If true, it will not froze a new form subission (after one is successfull) for half a day.
+    form_worker_url: URL to the cloudflare backend.
+  -->
+
+<script
+  src="https://cdn.jsdelivr.net/gh/tuhinpal/contact-form@master/src/contact-form.min.js"
+  id="contactform"
+  error_text=""
+  success_text=""
+  disable_waittime="true"
+  form_worker_url="https://contact-from.arupmandal731.workers.dev/"
+></script>
