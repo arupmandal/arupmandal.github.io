f8fc07cef8042a5c5c485d343dfd30110be1a5a7
Arup Mandal <110271890+cyberboyarup@users.noreply.github.com>
2023-04-09 17:01:15 +0530

    Arup Mandal

--- bot/app.js
+++
@@ -1,9 +0,0 @@
-const chatButton = document.querySelector('.chatbox__button');
-const chatContent = document.querySelector('.chatbox__support');
-const icons = {
-    isClicked: '<img src="./images/icons/chatbox-icon.svg" />',
-    isNotClicked: '<img src="./images/icons/chatbox-icon.svg" />'
-}
-const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
-chatbox.display();
-chatbox.toggleIcon(false, chatButton);
